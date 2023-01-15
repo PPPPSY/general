dcb67a7ef7eccd94ff34892ac6fbc1d48e70ccc7
PPPPSY <120995066+PPPPSY@users.noreply.github.com>
2023-01-15 20:38:14 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://www.surveycake.com/s/nZ3za',
+	'https://www.surveycake.com/s/6yr6W',
+	'https://www.surveycake.com/s/9qWvq'
 ];
